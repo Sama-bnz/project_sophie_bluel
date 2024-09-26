e87947d37d33469cc9db3e4e05afeb5ef66dc5b7
Benmezded Thomas <thomasbenmezded@gmail.com>
2024-09-26 21:23:54 +0200

    script_modals

--- js/app.js
+++ js/app.js
@@ -3,6 +3,12 @@ const gallery = document.getElementsByClassName("gallery");
 const category = document.getElementsByClassName("category");
 const authentication = document.getElementById("authentication");
 const favDialog = document.getElementById('favDialog');
+const favImageDialog = document.getElementById('favImageDialog');
+const closeModalWindow = document.getElementById('closeModal');
+const closePictureModal = document.getElementById('closePictureModal');
+const categorySelectBox = document.getElementById('categorySelectBox');
+const formDialog = document.getElementById('formDialog');
+
 
 async function fetchData (url) {
   try {
@@ -12,7 +18,6 @@ async function fetchData (url) {
     }
 
     const json = await response.json();
-    console.log(json);
     return json;
   } catch (error) {
     console.error(error.message);
@@ -21,7 +26,6 @@ async function fetchData (url) {
 
 async function loadWorks() {
     const url = "http://localhost:5678/api/works";
-    
     const result = await fetchData(url);
     return result;
 }
@@ -47,55 +51,44 @@ function templateCategory(category) {
 
 async function showWorks() {
     works = await loadWorks();
-    buildWorksTemplateList(works);
+    buildWorksTemplateList();
 }
-function buildWorksTemplateList(works) {
+function buildWorksTemplateList() {
   let template = "";
-    console.log(works);
     works.forEach((work) => {
-        console.log(work.title);
         template+=templateWork(work.title,work.imageUrl)
       });
-    console.log(template);
     gallery[0].innerHTML = template;
 }
 
 async function showCategories() {
   const categories = await loadCategory();
   let template = `<div onclick="loadWorkByCategory(event);" id="0" class="category_label">Tous</div>`;
-  console.log(categories);
   categories.forEach((category) => {
-      console.log(category.id);
       template+=templateCategory(category)
     });
-  console.log(template);
   template += `<button id="showModale">Modifier</button>`
   category[0].innerHTML = template;
   showModale();
 
 }
 function loadWorkByCategory(element){
-  console.log(element.target.id);
   let index = element.target.id;
-  console.log(works);
   if (index == 0){
     buildWorksTemplateList(works);
   }else{
     const worksFiltered = works.filter((work) => work.categoryId==index);
-    console.log(worksFiltered);
     buildWorksTemplateList(worksFiltered);
   }
   
 }
 function initAuthZone() {
   const token = localStorage.getItem("token");
-  console.log(token)
   let template = `<a href="./login.html">login</a>`;
   if(token!=undefined && token != null && token != "") {
     template = `<a href="#" id="logout">logout</a>`
   }
   authentication.innerHTML = template;
-  console.log(template);
   logout();
 }
 function logout() {
@@ -113,33 +106,139 @@ function logout() {
 }
 function showModale(){
   const buttonModale = document.getElementById('showModale');
-  buttonModale.addEventListener('click', () => {
+  buttonModale.addEventListener('click', async () => {
+    await loadImageModal();
     favDialog.showModal();
+    showPictureModal();
+    closeModal(closeModalWindow, favDialog);
+    });
+}
+async function loadImageModal(){
+ let template = '';
+ const works = await loadWorks();
+  works.forEach((work) => {
+      template+=templateModalImage(work)
+    });
+  formDialog.innerHTML = template;
+}
+function templateModalImage(work){
+  return `<div class="image-container">
+			<img src="${work.imageUrl}"  alt="1">
+			<i class="fa-solid fa-trash-can trash-icon" ></i>
+		  </div>`
+}
+const addPictureButton = document.getElementById('validatePicture');
+
+function showPictureModal() {
+  const buttonModale = document.getElementById('addPictureDialog');
+  buttonModale.addEventListener('click', () => {
+    favImageDialog.showModal();
+    closeModal(closePictureModal, favImageDialog);
+    fillSelectCategory();
+    loadImage();
+    addPictureButton.addEventListener('click', addPictureEvent);
   });
 }
 
-document.getElementById('photo').addEventListener('change', function(event) {
-  // Récupère le fichier sélectionné
-  const file = event.target.files[0]; 
-  // Sélectionne l'élément d'aperçu
-  const preview = document.getElementById('photoPreview'); 
-  // Le bouton d'ajout de photo
-  const label = document.getElementById('photoLabel'); 
+function closeModal(buttonClose, dialog){
+  buttonClose.addEventListener('click',() => {
+    dialog.close();
+  })
+}
+async function fillSelectCategory() {
+  const categories = await loadCategory();
+  let template = `<option value=""></option>`
+  categories.forEach((category) => {
+    template+=templateOptionCategory(category)
+  });
+  categorySelectBox.innerHTML = template;
+}
 
-  if (file) {
-      const reader = new FileReader();
-      reader.onload = function(e) {
-          // Change le style de fond du conteneur d'aperçu
-          preview.style.backgroundImage = `url(${e.target.result})`;
-          // Montre l'aperçu
-          preview.style.display = 'block'; 
-          // Cache le bouton d'ajout de photo
-          label.style.display = 'none'; 
-      }
-      // Lit l'image comme une URL data
-      reader.readAsDataURL(file); 
+function templateOptionCategory(category){
+  return `<option value="${category.id}" class="category_label">${category.name}</option>`;
+}
+const pictureTitle = document.getElementById('title');// titre modale photo
+
+async function addPictureEvent(event){
+  event.preventDefault();
+  const title = pictureTitle.value;
+  const category = categorySelectBox.value;
+  const photo = document.querySelector("#photo");
+  console.log(photo.files[0]);
+  console.log(title);
+  console.log(category);
+  const response = await postWork({
+    title : title,
+    image : photo.files[0],
+    category : category
+  })
+  console.log(response);
+  if(response.ok){
+    favImageDialog.close('ok');
   }
-});
+}
+
+function loadImage(){
+  document.getElementById('photo').addEventListener('change', function(event) {
+    // Récupère le fichier sélectionné
+    const file = event.target.files[0]; 
+    // Sélectionne l'élément d'aperçu
+    const preview = document.getElementById('photoPreview'); 
+    // Le bouton d'ajout de photo
+    const label = document.getElementById('photoLabel'); 
+  
+    if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+            // Change le style de fond du conteneur d'aperçu
+            preview.style.backgroundImage = `url(${e.target.result})`;
+            // Montre l'aperçu
+            preview.style.display = 'block'; 
+            // Cache le bouton d'ajout de photo
+            label.style.display = 'none'; 
+        }
+        // Lit l'image comme une URL data
+        reader.readAsDataURL(file); 
+    }
+  });
+}
+
+async function postWork(work){
+  const formData = new FormData();
+  const token = localStorage.getItem("token");
+  const headers = { 'Authorization': `Bearer ${token}`};
+  formData.append("title", work.title);
+  formData.append("category", work.category);
+  formData.append("image", work.image);
+  const response = await fetch("http://localhost:5678/api/works", {
+    method: "POST",
+    body: formData,
+    headers:headers
+  });
+  return response;
+}
+function reloadModal() {
+  favImageDialog.addEventListener("close", async (e) => {
+    console.log(favImageDialog.returnValue);
+    if(favImageDialog.returnValue == 'ok'){
+      await loadImageModal();
+      favDialog.returnValue = 'ok';
+    }
+  });
+}
+
+function reloadWork(){
+  favDialog.addEventListener("close",async (e) => {
+    console.log("bonjour");
+    console.log(favDialog.returnValue);
+    if(favDialog.returnValue == 'ok'){
+      await showWorks();
+      // favDialog.returnValue = 'ok';
+    }
+  });
+}
+reloadWork();
+reloadModal();
 showWorks();
 showCategories();
 initAuthZone();
